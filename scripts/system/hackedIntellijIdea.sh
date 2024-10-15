67cd4dfaa357038ea718d798a38a564ca93eca40
lclHckr <localHckr@proton.me>
2024-10-15 04:06:23 +0300

    added .config file

---
+++ scripts/system/hackedIntellijIdea.sh
@@ -0,0 +1,2 @@
+___MY_VMOPTIONS_SHELL_FILE="${HOME}/.jetbrains.vmoptions.sh"; if [ -f "${___MY_VMOPTIONS_SHELL_FILE}" ]; then . "${___MY_VMOPTIONS_SHELL_FILE}"; fi
+bash ~/programs/idea-IU-242.23339.11/bin/idea.sh
